171a43c01ece0db13473a4765bb54b585267a686
mdfahim50 <mdfahim.info@gmail.com>
2020-08-18 21:47:29 +0600

    hard rock

---
+++ main.js
@@ -0,0 +1,32 @@
+const inputtedInfo = document.getElementById('inputted-info');
+const songList = document.getElementById('song-list');
+/*fetching song info and making list of ten item */
+function songSearch() {
+    fetch('https://api.lyrics.ovh/suggest/' + inputtedInfo.value)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            const fetchSongItem = () => {
+                songList.innerHTML = '';
+
+                for (var i = 0; i < 10; i++) {
+                    songList.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
+                                                <div class="col-md-5">
+                                                    <h3 class="lyrics-name" id= 'song-title-${i}'>${json.data[i].title}</h3>
+                                                    <p class="author lead">Album by <span>${json.data[i].album.title}</span></p>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <h3 class="">Artist</h3>
+                                                    <p class="artist-name lead" id='artist-name-${i}'> ${json.data[i].artist.name}</p>
+                                                </div>
+                            
+                                                <div class="col-md-3 text-md-right text-center">
+                                                    <button class="btn btn-success"  onclick="lyricSearch(${i})">Get Lyrics</button>
+                                                </div>
+                                            </div>`
+                }
+            };
+            fetchSongItem();
+        });
+};
+
